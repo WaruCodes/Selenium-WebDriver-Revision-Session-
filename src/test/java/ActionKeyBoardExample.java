1e90f6b5e344b795f86d568d27ee22f125c71c39
WaruCodes <sdrwarushika@std.appsc.sab.ac.lk>
2026-05-25 12:27:42 +0530

    KeyBoard acrion test cases and executed and passed

--- src/test/java/ActionKeyBoardExample.java
+++ src/test/java/ActionKeyBoardExample.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static javax.swing.UIManager.get;
+
 public class ActionKeyBoardExample {
     WebDriver driver;
 
@@ -21,11 +25,11 @@ public class ActionKeyBoardExample {
     public void keyBoardActionsTest1() throws InterruptedException {
         driver.get("https://www.google.com/");
         WebElement googleSearchTextBox = driver.findElement(By.name("q"));
-        googleSearchTextBox.sendKeys("WelCome");
+        //googleSearchTextBox.sendKeys("WelCome");
 
         //For the Action class create a method
         Actions actions = new Actions(driver);
-        Action storeBuildOperation = actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build();
+        /*Action storeBuildOperation = actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build();
         storeBuildOperation.perform(); // if not we can use perform directly without store the build operation in the action
 
         Thread.sleep(5000);
@@ -38,13 +42,30 @@ public class ActionKeyBoardExample {
                 .keyUp(Keys.CONTROL)
                 .keyDown(Keys.CONTROL)
                 .sendKeys("x")
-                .build().perform();
+                .build().perform();*/
 
         //To write in capital in the text box using JAVA method overloading concept
+        actions.keyDown(googleSearchTextBox, Keys.SHIFT)
+                .sendKeys("I am a QA engineer at laughs holdings")
+                .perform();
+
     }
 
     @Test
-    public void keyBoardActionsTest2(){
-        driver.get("https://www.leafground.com/link.xhtml");
+    public void keyBoardActionsTest2() throws InterruptedException {
+        driver.get("https://www.leafground.com/list.xhtml");
+
+        List<WebElement> selectable = driver.findElements(By.xpath("//ul[@aria-label='From']/li"));
+        int size = selectable.size();
+        System.out.println("Li count is : " + size);
+        Thread.sleep(4000);
+
+
+        Actions action = new Actions(driver);
+        action.keyDown(Keys.CONTROL)
+                .click(selectable. get(0)) //click the 1st element in the selectable list
+                .click(selectable. get(1))
+                .click(selectable. get(2))
+                .perform();
     }
 }
