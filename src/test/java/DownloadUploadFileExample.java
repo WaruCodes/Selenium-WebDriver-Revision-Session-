9b1f9f301c06a3f48152d816999c070db63fdc74
WaruCodes <sdrwarushika@std.appsc.sab.ac.lk>
2026-05-26 20:21:43 +0530

    Download & Upload file example test cases executed and passed

--- src/test/java/DownloadUploadFileExample.java
+++ src/test/java/DownloadUploadFileExample.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class DownloadUploadFileExample {
@@ -33,6 +36,40 @@ public class DownloadUploadFileExample {
                 break;
             }
         }
+
+    }
+
+    @Test
+    public void fileDownloadTest2() throws InterruptedException, AWTException {
+        driver.get("https://www.leafground.com/file.xhtml");
+
+        //1st Way using Robot Class
+        WebElement uploadBtn = driver.findElement(By.id("j_idt88:j_idt89"));
+        uploadBtn.click();
+
+        //Windows control begins here
+        String data = "F:\\Downloads\\download.jpg";
+        StringSelection selection = new StringSelection(data);
+        //Coping the path to the clipboard
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null); // default toolkit eke thiyena clipboard eka gattha
+
+        Thread.sleep(4000);
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent. VK_CONTROL);
+        robot.keyPress(KeyEvent. VK_V);
+        robot.keyRelease(KeyEvent. VK_V);
+        robot.keyRelease(KeyEvent. VK_CONTROL);
+
+        Thread.sleep(3000);
+        robot.keyPress(KeyEvent. VK_ENTER);
+        robot.keyRelease(KeyEvent. VK_ENTER);
+
+        //2nd way - Using sendkeys (Applicable only element type is file) - input type - file
+        /*String data = "F:\\Downloads\\download.jpg";
+        WebElement uploadUsingSendKeys = driver.findElement(By.id("j_idt88:j_idt89_input"));
+        uploadUsingSendKeys.sendKeys(data);*/
+
+
 
     }
 }
